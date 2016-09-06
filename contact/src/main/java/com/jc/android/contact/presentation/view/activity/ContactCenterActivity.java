fa7acf4d6f9b52518a6711f6d420fa5480c9940f
Tconan99 <627594232@qq.com>
2016-09-06 17:15:29 +0800

    我也不知道改了什么

--- contact/src/main/java/com/jc/android/contact/presentation/view/activity/ContactCenterActivity.java
+++ contact/src/main/java/com/jc/android/contact/presentation/view/activity/ContactCenterActivity.java
@@ -4,21 +4,24 @@ import android.os.Bundle;
 import android.view.Menu;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.jc.android.contact.presentation.model.ContactModel;
 import com.jc.android.contact.presentation.view.fragment.ContactListFragment;
 import com.jc.android.module.contact.R;
 import com.jc.android.widget.presentation.view.activity.BackActivity;
 import com.jc.android.widget.presentation.view.widget.ActivityUtils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContactCenterActivity extends BackActivity {
+
+    private Map<Long, ContactModel> selected = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fresco.initialize(this);
         setContentView(R.layout.activity_contact_center);
-
-        //findViewById()
-
 
         ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), new ContactListFragment(), R.id.container);
     }
